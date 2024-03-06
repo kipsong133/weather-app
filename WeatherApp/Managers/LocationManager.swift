9be63b33207b00df2189527cba7e6fc7cadfffd3
KimWooSung <kipsong133@gmail.com>
2024-03-07 08:17:27 +0900

    first commit

---
+++ WeatherApp/Managers/LocationManager.swift
@@ -0,0 +1,13 @@
+//
+//  LocationManager.swift
+//  WeatherApp
+//
+//  Created by 아프로 on 2024/03/07.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    
+}
---
+++ WeatherApp/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by 아프로 on 2024/03/07.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
