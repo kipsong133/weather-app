651caa8b9aa46b65080eef3073bb3b637ff27a8a
KimWooSung <kipsong133@gmail.com>
2024-03-07 14:37:49 +0900

    Fix: 현재위치에서 제주도 위치로 변경

--- WeatherApp/Managers/WeatherManager.swift
+++ WeatherApp/Managers/WeatherManager.swift
@@ -6,7 +6,10 @@ import CoreLocation
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
         /// [apiKey] 값은 [https://openweathermap.org/] 에서 발급받은 API Key로 대체해야 합니다.
-        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric") else { fatalError("Missing URL") }
+        /// 주석처리된 코드는 현재 위치
+//        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric") else { fatalError("Missing URL") }
+        /// 제주도
+        guard let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=33.4890&lon=126.4983&appid=\(apiKey)&units=metric") else { fatalError("Missing URL") }
 
         let urlRequest = URLRequest(url: url)
 
