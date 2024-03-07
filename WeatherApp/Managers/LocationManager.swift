f5aede3666aa8bc38f9224ae55e68e56426cf812
KimWooSung <kipsong133@gmail.com>
2024-03-07 11:08:33 +0900

    Feat: 위치정보 및 날씨정보 획득을 위한 객체 생성
    - LocationManager: "CoreLocation"을 활용한 위치정보 담당 객체
    - WeatherManager: "OpenWeatherAPI"를 활용한 날씨정보 조회 담당 객체
    - ResonseBody: OpenWeatherAPI에서 주는 데이터 모델

--- WeatherApp/Managers/LocationManager.swift
+++ WeatherApp/Managers/LocationManager.swift
@@ -1,13 +1,34 @@
-//
-//  LocationManager.swift
-//  WeatherApp
-//
-//  Created by 아프로 on 2024/03/07.
-//
+/**
+ docs: https://developer.apple.com/documentation/corelocation/configuring_your_app_to_use_location_services
+ */
+
 
 import Foundation
 import CoreLocation
 
 class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
     
+    @Published var location: CLLocationCoordinate2D?
+    @Published var isLoading = false
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestLocation() {
+        isLoading = true
+        manager.requestLocation()
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        location = locations.first?.coordinate
+        isLoading = false
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        Logger.log("위처정보 획득 에러: \(error)")
+        isLoading = false
+    }
 }
